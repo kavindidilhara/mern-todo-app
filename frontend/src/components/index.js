b9412f28b571ea8ab9ef8d8ff602491557752a56
SusalSandeepa <susalsandeepa2002@gmail.com>
2025-07-27 18:26:30 +0530

    Styling The Form Component & Preparing For The Functionality
    Styling The Form Component & Preparing For The Functionality

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Container } from "./styles";
+import Form from "./Form";
 
 function Todo() {
   return (
     <Container>
       <h2>List of To do</h2>
       {/*Form component*/}
+      <Form />
       {/*To do List*/}
       {/*Key*/}
       {/*Author component*/}
