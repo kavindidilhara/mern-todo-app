169d4aba706a393326fedcaad9eca412be78bde5
kavindidilhara <kavidil28@gmail.com>
2025-07-30 17:17:29 +0530

    backend updated

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,50 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const Cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const mongoose = require("mongoose");
+const Cors = require("cors");
+const dotenv = require("dotenv");
 
-dotenv.config()
+dotenv.config();
+
+const {
+  getTodos,
+  createTodo,
+  updateTodo,
+  deleteTodo,
+} = require("./controllers/todoController");
 
 // App config
 const app = express();
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-const connectionURL = process.env.MONGO_URI
+const connectionURL = process.env.MONGO_URI;
 
 // Middlewares
 //Convert to json
-app.use(express.json())
+app.use(express.json());
 
-app.use(Cors())
+app.use(Cors());
 
 //DB Config
-mongoose.connect(connectionURL)
-.then(() => {
-    app.listen(port, () => console.log(`Running on port: ${port}`))
-})
-.catch((err)=>{
+mongoose
+  .connect(connectionURL)
+  .then(() => {
+    app.listen(port, () => console.log(`Running on port: ${port}`));
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
+
+//API Endpoints
+
+//Get todo list
+app.get("/todos", getTodos);
+
+//Create a new todo
+app.post("/todos", createTodo);
+
+//Update a todo
+app.put("/todos/:id", updateTodo);
+
+//Delete a todo
+app.delete("/todos/:id", deleteTodo);
