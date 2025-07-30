2dd6f5fab1e352693d4ef8bb1dcdadd93b3396eb
SusalSandeepa <susalsandeepa2002@gmail.com>
2025-07-30 18:05:32 +0530

    Backend Update
    fetch data from the api

--- frontend/src/components/Todo/index.js
+++ frontend/src/components/Todo/index.js
@@ -1,10 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Container } from "./styles";
 import Form from "../Form";
+import axios from "../../axios"
 
 function Todo() {
   const [input, setInput] = useState("");
-  console.log(input, "input");
+  const [todos, setTodos] = useState([]);
+
+  //console.log(input, "input");
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get("/todos")
+      setTodos(response.data);
+    } catch (err) {
+      console.log(err.message)
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const addTodo = async () => {
+    console.log('addedTodo')
+  }
+
+  console.log(todos, "todos");
+
   return (
     <Container>
       <h2>List of To do</h2>
