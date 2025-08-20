0d4df73787981c84e44810afd720a76a1e6c7a82
SusalSandeepa <susalsandeepa2002@gmail.com>
2025-08-20 19:51:06 +0530

    Finish The Todo List With Update & Delete Request To MongoDB

---
+++ frontend/src/components/TodoList/index.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import axios from '../../axios';
+import { ListContainer, Row, Text, DeleteIcon } from './styles';
+
+function TodoList({todos, fetchData}) {
+    console.log(todos, "hi");  
+    
+    const updateTodo = async (id) => {
+        try {
+            const response = await axios.put(`/todos/${id}`, {
+                id,
+            });
+            fetchData();
+
+            return response.data.json;
+            
+        } catch(err) {
+            console.error(err.message);
+        }
+    };
+
+    const deleteTodo = async (id) => {
+        try {
+            const response = await axios.delete(`/todos/${id}`, {
+                id,
+            });
+            fetchData();
+
+            return response.data.json;
+            
+        } catch(err) {
+            console.error(err.message);
+        }
+    };
+
+  return (
+    <div>
+        <ListContainer>
+            {/* render todos in bullet points */}
+            {todos?.map((todo)=> (
+                <Row key={todo._id}>
+                    <Text 
+                    onClick={() => updateTodo(todo._id)}
+                    isCompleted={todo.completed}
+                    >
+                        {todo.text}
+                    </Text>
+                    <DeleteIcon onClick={() => deleteTodo(todo._id)}>X</DeleteIcon>
+                </Row>
+            ))}
+        </ListContainer>
+    </div>
+  )
+}
+
+export default TodoList
